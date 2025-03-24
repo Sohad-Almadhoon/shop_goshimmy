eb2321c03a2217bc0a890f4cbb65a9258fb2ccb2
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2025-03-24 17:52:49 +0200

    organize code

--- components/profile/account/dialogs/CancelMembershipDialog.tsx
+++ components/profile/account/dialogs/CancelMembershipDialog.tsx
@@ -11,7 +11,8 @@ import {
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
-import { cancelMembership } from "@/helpers/mockData";
+import { cancelMembership } from "@/helpers/constants";
+
 import React from "react";
 import { BiX } from "react-icons/bi";
 
--- components/profile/account/dialogs/CancelThanksDialog.tsx
+++ components/profile/account/dialogs/CancelThanksDialog.tsx
@@ -1,28 +1,30 @@
 import {
   AlertDialog,
-  AlertDialogAction,
+  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
+  AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 import React from "react";
 
 const CancelThanksDialog = () => {
   return (
-    <AlertDialog open={true}>
+    <AlertDialog open={false}>
       <AlertDialogContent className="lg:max-w-md max-w-md p-6 rounded-lg bg-white text-black">
         <AlertDialogHeader>
+          <AlertDialogTitle className=" sr-only">Hi</AlertDialogTitle>
           <AlertDialogDescription className="text-xl font-bold">
             Thanks for letting us know! You can rejoin Ensemble at any time.
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          <AlertDialogAction>Close</AlertDialogAction>
+          <AlertDialogCancel className="bg-primary">Close</AlertDialogCancel>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
   );
 };
 
-export default  CancelThanksDialog;
+export default CancelThanksDialog;
